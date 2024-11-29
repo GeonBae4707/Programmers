d7a7039b4fc364106c1be4b99db2e6fbe3f8198f
vkis011 <vkis011@naver.com>
2024-11-29 16:41:18 +0900

    change project setting

---
+++ ConsoleApp/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            Solution test = new Solution();
+            //test.solution("aukks", "wbqd", 5);
+            test.solution(new int[] { 2, 6, 8, 14 });
+
+            Console.ReadKey();
+        }
+    }
+}
